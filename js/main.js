be31deeec74985695313ecea032dc800933d1561
Vaycheslav <slavonkozak@mail.ru>
2022-10-03 00:39:27 +0300

    end layout and new function scrollIntoView

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const items = document.querySelectorAll('.menu__item')
+
+items.forEach(item => {
+  item.addEventListener('click', function(e){
+    e.preventDefault()
+    let num = item.getAttribute('href')
+    let elem = document.querySelector(num)
+    elem.scrollIntoView({
+      behavior: 'smooth'
+    })
+  })
+})
